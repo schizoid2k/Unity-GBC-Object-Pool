595e0f39b8608cb5bc895ac9f5271b90782036e8
John Schumacher <john@candlelight.com>
2018-04-13 10:04:54 -0400

    Initial Load

---
+++ Assets/GBC Object Pooler/Demo/Scripts/DemoScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoScript : MonoBehaviour {
+
+    public Text CubesText;
+    public Text SpheresText;
+
+    private void Start() {
+        CubesText.text = "Cubes In Pool: " + GBCObjectPooler.Instance.ItemsInPool("Cube");
+        SpheresText.text = "Spheres In Pool: " + GBCObjectPooler.Instance.ItemsInPool("Sphere");
+    }
+
+    // Use this for initialization
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            Vector2 mousePos = Input.mousePosition;
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo)) {
+                if (hitInfo.collider.name.Equals("Cube")) {
+                    GBCObjectPooler.Instance.ReturnToPool(hitInfo.collider.gameObject, "Cube");
+                }
+            }
+            else {
+                GameObject go = GBCObjectPooler.Instance.GetPooledObject("Cube");
+                go.transform.position = new Vector3(pos.x, pos.y, Random.Range(0f, -1f));
+                go.SetActive(true);
+            }
+
+            Debug.Log("Cubes Left in Pool: " + GBCObjectPooler.Instance.ItemsInPool("Cube") +
+                "     Spheres Left in Pool: " + GBCObjectPooler.Instance.ItemsInPool("Sphere"));
+
+            CubesText.text = "Cubes In Pool: " + GBCObjectPooler.Instance.ItemsInPool("Cube");
+        }
+
+        if (Input.GetMouseButtonUp(1)) {
+            Vector2 mousePos = Input.mousePosition;
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo)) {
+                if (hitInfo.collider.name.Equals("Sphere")) {
+                    GBCObjectPooler.Instance.ReturnToPool(hitInfo.collider.gameObject, "Sphere");
+                }
+            }
+            else {
+                GameObject go = GBCObjectPooler.Instance.GetPooledObject("Sphere");
+                go.transform.position = new Vector3(pos.x, pos.y, Random.Range(0f, -1f));
+                go.SetActive(true);
+            }
+
+            Debug.Log("Cubes Left in Pool: " + GBCObjectPooler.Instance.ItemsInPool("Cube") +
+                "     Spheres Left in Pool: " + GBCObjectPooler.Instance.ItemsInPool("Sphere"));
+
+            SpheresText.text = "Spheres In Pool: " + GBCObjectPooler.Instance.ItemsInPool("Sphere");
+        }
+
+
+    }
+}
---
+++ Assets/GBC Object Pooler/Scripts/GBCObjectPooler.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * GBC Object Pool for Unity
+ * Originally Developed by John Schumacher
+ * http://gamesbycandlelight.com
+ * 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GBCObjectPooler : MonoBehaviour {
+
+    [System.Serializable]
+    public class Pool {
+        public GameObject PooledObject;
+        public int PooledAmount;
+        public string PoolName;
+    }
+
+    public static GBCObjectPooler Instance;
+    public List<Pool> Pools;
+
+    private Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Awake() {
+        if (Instance != null && Instance != this) Destroy(gameObject);
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in Pools) {
+            // Create a game object for each pool
+            // Keeps it clean
+            GameObject go = new GameObject(pool.PoolName + " Pool");
+            go.transform.SetParent(transform, false);
+
+            // Create an object pool queue
+            Queue<GameObject> objectPoolQueue = new Queue<GameObject>();
+
+            // Populate
+            for (int i = 0; i < pool.PooledAmount; i++) {
+                GameObject pooledObject = (GameObject)Instantiate(pool.PooledObject, Vector3.zero, Quaternion.identity, go.transform);
+                pooledObject.name = pool.PoolName + " (Inactive)";
+                pooledObject.SetActive(false);
+                objectPoolQueue.Enqueue(pooledObject);
+            }
+
+            poolDictionary.Add(pool.PoolName, objectPoolQueue);
+        }
+	}
+
+    /// <summary>
+    /// Gets an unused object from the specified pool
+    /// </summary>
+    /// <param name="PoolName">Name of Pool</param>
+    /// <returns>An unused object or null if there is none available</returns>
+    public GameObject GetPooledObject(string PoolName) {
+        if (!poolDictionary.ContainsKey(PoolName)) return null;
+        if (poolDictionary[PoolName].Count <= 0) return null;
+        GameObject go = poolDictionary[PoolName].Dequeue();
+        go.name = PoolName;
+        return go;
+    }
+
+    /// <summary>
+    /// Returns the item back to the appropriate pool
+    /// </summary>
+    /// <param name="go">GameObject to return to pool</param>
+    /// <param name="PoolName">Name of Pool</param>
+    public bool ReturnToPool(GameObject go, string PoolName) {
+        if (!poolDictionary.ContainsKey(PoolName)) return false;
+
+        go.name = PoolName + " (Inactive)";                             
+        poolDictionary[PoolName].Enqueue(go);
+        go.SetActive(false);
+        return true;
+    }
+
+    /// <summary>
+    /// Returns number of unused objects in the specified pool
+    /// </summary>
+    /// <param name="PoolName">Name of Pool</param>
+    /// <returns>Number of object in pool, or null if pool does not exist</returns>
+    public int? ItemsInPool(string PoolName) {
+        if (!poolDictionary.ContainsKey(PoolName)) return null;
+        return poolDictionary[PoolName].Count;
+    }
+}
